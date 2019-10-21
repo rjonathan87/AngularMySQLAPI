605e439f4b218f9b66b93203f95be51bcf33fa86
rjonathan87 <rjonathan87@gmail.com>
2019-10-21 17:22:50 -0500

    CRUD API Complet

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -1,23 +1,36 @@
 import { Request, Response } from "express";
 import pool from "../database";
+import { json } from "body-parser";
 
 class GamesController {
-  public async list(req: Request, res: Response) {
+  public async list(req: Request, res: Response): Promise<any> {
     try {
-
       await pool.query('SELECT * FROM games', (err, result, fields) => {
         if (err) throw err;
-        res.json(result);
+
+        if (result.length > 0)
+          return res.json(result);
+        else
+          return res.json({ message: 'No hay juegos que mostrar' });
       });
 
     } catch (error) {
       console.log(error);
-
     }
   }
 
-  public getOne(req: Request, res: Response) {
-    res.json({ message: `This is game ${req.params.id}` });
+  public async getOne(req: Request, res: Response): Promise<void> {
+    //destructuracion 
+    const { id } = req.params;
+    // const games = await pool.query('SELECT * FROM games WHERE id = ?', [id]);
+    await pool.query('SELECT * FROM games WHERE id = ?', [id], (err, result, fields) => {
+      if (err) throw err;
+
+      if (result.length > 0) {
+        return res.json(result[0]);
+      }
+      return res.status(404).json({ message: 'El juego no existe' });
+    })
   }
 
   public async create(req: Request, res: Response): Promise<void> {
@@ -28,12 +41,24 @@ class GamesController {
     res.json({ message: "Game saved" });
   }
 
-  public put(req: Request, res: Response) {
-    res.json({ message: `Updating a game ${req.params.id}` });
+  public async put(req: Request, res: Response) {
+    // res.json({ message: `Updating a game ${req.params.id}` });
+    const { id } = req.params;
+    await pool.query('UPDATE games SET ? WHERE id = ?', [req.body, id], (err, result, fields) => {
+      if (err) throw err;
+
+      return res.json({ message: 'Juego actualizado' });
+    })
   }
 
-  public delete(req: Request, res: Response) {
-    res.json({ message: `Deleting a game ${req.params.id}` });
+  public async delete(req: Request, res: Response): Promise<void> {
+    // res.json({ message: `Deleting a game ${req.params.id}` });
+    const { id } = req.params;
+    await pool.query('DELETE FROM games WHERE id = ?', [id], (err, result, fields) => {
+      if (err) throw err;
+
+      return res.json({ message: 'Juego eliminado' });
+    });
   }
 }
 const gamesController = new GamesController();
