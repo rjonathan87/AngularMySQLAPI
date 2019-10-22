98dd11609f465c0b648cd9ec98094f968ddc007e
rjonathan87 <rjonathan87@gmail.com>
2019-10-22 18:17:19 -0500

    add Links Menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GamesListComponent } from './components/pages/games/games-list/games-list.component';
+import { GamesFormComponent } from './components/pages/games/games-form/games-form.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'games',
     component: GamesListComponent
+  },
+  {
+    path: 'add',
+    component: GamesFormComponent
   }
 ];
 
