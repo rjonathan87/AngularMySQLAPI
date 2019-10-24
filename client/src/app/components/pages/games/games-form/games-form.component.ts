71738e3471b38acf3fae7c679d066b86506be45d
rjonathan87 <rjonathan87@gmail.com>
2019-10-23 21:18:45 -0500

    NgFormValid

--- client/src/app/components/pages/games/games-form/games-form.component.ts
+++ client/src/app/components/pages/games/games-form/games-form.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Games } from 'src/app/models/Games';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { Games } from "src/app/models/Games";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-games-form',
-  templateUrl: './games-form.component.html',
-  styleUrls: ['./games-form.component.css']
+  // tslint:disable-next-line: quotemark
+  selector: "app-games-form",
+  templateUrl: "./games-form.component.html",
+  styleUrls: ["./games-form.component.css"]
 })
 export class GamesFormComponent implements OnInit {
-
   game: Games = {
     id: 0,
-    title: '',
-    description: '',
-    image: '',
+    title: "",
+    description: "",
+    image: "",
     created_at: new Date()
   };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(frm: NgForm) {
+    console.log(frm.value); // { first: '', last: '' }
+    console.log(frm.valid); // false
   }
-
-
 }
